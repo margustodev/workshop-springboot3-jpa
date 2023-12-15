347a7cebff4b6590de42730a46e7ff6b25a0bd4c
Marcelo Augusto <margustoadm@hotmail.com>
2023-12-15 10:53:19 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,6 +1,5 @@
 package com.educandoweb.course.resources;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
